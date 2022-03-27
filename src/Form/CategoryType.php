ea247330c6d0dfbf043819157fad4973047c5a55
Timothe.L <timothe463@icloud.com>
2022-03-27 19:07:03 +0200

    Crud Categ => bug EntityType / Front

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label', TextType::class,
+                [
+                    'required'=>true,
+                    'attr'=>['class'=>'form-control']
+                ]
+            )
+            ->add('cat_parent', EntityType::class,
+                [
+                    'attr'=>['class'=>'form-control']
+                ]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
