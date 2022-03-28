ef172c00215c06f533b9f8d85daffea107884c8e
Timothe.L <timothe463@icloud.com>
2022-03-28 12:07:06 +0200

    admin --> security

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,12 +6,15 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/admin', name: 'admin_')]
+/*#[IsGranted('ROLE_ADMIN')]*/
+
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'index')]
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -30,7 +30,22 @@ class UserFixtures extends Fixture
         $admin->setAddressLine('esplanade de Russi');
         $admin->setPostCode('63110');
         $admin->setCity('Beaumont');
+        $admin->setRoles(['ROLE_ADMIN']);
 
+        $client = new User();
+
+        $client->setFirstname('Marc');
+        $client->setLastname('Marc');
+        $client->setEmail('marc@marc.marc');
+        $client->setPassword($this->hasher->hashPassword($admin,'marc'));
+        $client->setGenre('male');
+        $client->setBirthday('13/02/2000');
+        $client->setNbStreet('14');
+        $client->setAddressLine('esplanade de Kosovi');
+        $client->setPostCode('63110');
+        $client->setCity('Bellomont');
+        $client->setRoles(['ROLE_USER']);
+        $manager->persist($client);
         $manager->persist($admin);
 
         $manager->flush();
