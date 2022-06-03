657e561062bf7aecae8b600238510770efa78bb8
Timothe.L <timothe463@icloud.com>
2022-06-03 18:00:29 +0200

    admin_front-next

--- public/js/admin/admin_header.js
+++ public/js/admin/admin_header.js
@@ -1,30 +1,68 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+const contentLayout = document.querySelector('#content-layout');
+const navBar = document.querySelector('#nav-bar');
 
-    const showNavbar = (toggleId, navId, bodyId, headerId) => {
-        const toggle = document.getElementById(toggleId),
-            nav = document.getElementById(navId),
-            bodypd = document.getElementById(bodyId),
-            headerpd = document.getElementById(headerId)
+const showNavbar = (toggleOpenId, toggleCloseId, navId, bodyId, headerId, layoutId) => {
+    const toggleOpen = document.getElementById(toggleOpenId),
+        toggleClose = document.getElementById(toggleCloseId),
+        nav = document.getElementById(navId),
+        bodypd = document.getElementById(bodyId),
+        headerpd = document.getElementById(headerId),
+        contentLayout = document.getElementById(layoutId)
 
 // Validate that all variables exist
-        if (toggle && nav && bodypd && headerpd) {
-            toggle.addEventListener('click', () => {
+    if (toggleOpen && nav && bodypd && headerpd && toggleClose && contentLayout) {
+        toggleOpen.addEventListener('click', () => {
 // show navbar
-                nav.classList.toggle('show')
+            nav.classList.toggle('show')
 // change icon
-                toggle.classList.toggle('bx-x')
-// add padding to body
-                bodypd.classList.toggle('body-pd')
+            toggleOpen.classList.toggle('bx-x')
 // add padding to header
-                headerpd.classList.toggle('header-pd')
-            })
-        }
+            headerpd.classList.toggle('header-pd')
+// add display to div ContentLayout
+            contentLayout.classList.toggle('d-none')
+// add opacity to div ContentLayout
+            contentLayout.classList.toggle('op')
+        });
+        toggleClose.addEventListener('click', () => {
+// show navbar
+            nav.classList.toggle('show')
+// change icon
+            toggleClose.classList.toggle('bx-x')
+// add padding to header
+            headerpd.classList.toggle('header-pd')
+// add display to div ContentLayout
+            contentLayout.classList.toggle('d-none')
+// add opacity to div ContentLayout
+            contentLayout.classList.toggle('op')
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    if (innerWidth > 768 && !contentLayout.classList.contains('d-none')) {
+        contentLayout.classList.add('d-none');
     }
 
-    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+    window.addEventListener("resize", () => {
+        if (innerWidth > 768 && !contentLayout.classList.contains('d-none')) {
+            contentLayout.classList.add('d-none');
+        } else if (innerWidth < 768 && navBar.classList.contains('show')) {
+            contentLayout.classList.remove('d-none');
+        }
+    });
+
+    contentLayout.addEventListener('click', ()=>{
+        console.log('gdg');
+
+    })
+
+
+
+    showNavbar('header-open', 'header-close', 'nav-bar', 'body-pd', 'header', 'content-layout');
 
     /*===== LINK ACTIVE =====*/
-    const linkColor = document.querySelectorAll('.nav_link')
+    const linkColor = document.querySelectorAll('.nav_link');
 
     function colorLink() {
         if (linkColor) {
@@ -33,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         }
     }
 
-    linkColor.forEach(l => l.addEventListener('click', colorLink))
+    linkColor.forEach(l => l.addEventListener('click', colorLink));
 
 // Your code to run since DOM is loaded and ready
 });
